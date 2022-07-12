e11dcefaff5023e36e85586b68c58f0dc198dd7e
Hubert Lemczak <hubertlemczak@gmail.com>
2022-07-12 10:02:14 +0100

    routing

---
+++ src/components/categories/categories.component.jsx
@@ -0,0 +1,13 @@
+import './categories.styles.scss';
+
+import CategoryItem from '../category-item/category-item.component';
+
+const Categories = ({ categories }) => (
+  <div className="categories-container">
+    {categories.map((category) => (
+      <CategoryItem key={category.id} category={category} />
+    ))}
+  </div>
+);
+
+export default Categories;
---
+++ src/routes/sign-in/sign-in.component.jsx
@@ -0,0 +1,13 @@
+import { Fragment } from 'react';
+
+import './sign-in.styles.scss';
+
+const SignIn = () => {
+  return (
+    <Fragment>
+      <div>hello sign in pls</div>
+    </Fragment>
+  );
+};
+
+export default SignIn;
